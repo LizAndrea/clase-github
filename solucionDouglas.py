b7ddcfcc06bee0d5ca837a9b556aefb0b50f64c8
Douglashc <douglash.dcz@gmail.com>
2025-02-09 17:54:57 -0400

    Solucion de Douglas

---
+++ solucionDouglas.py
@@ -0,0 +1,8 @@
+def area(base, altura):
+    return (base * altura)
+
+def perimetro(base, altura):
+    return (2 * (base + altura))
+
+print(f"La base es: {area(2, 5)}")
+print(f"La altura es: {perimetro(4, 5)}")
