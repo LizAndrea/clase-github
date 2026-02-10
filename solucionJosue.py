0c209c68bdf6dbb279485a0d873a1046fcabf872
josuechilali <josue.chilali@uab.edu.bo>
2026-02-09 20:24:05 -0400

    Refactorizacion Python

---
+++ solucionJosue.py
@@ -0,0 +1,17 @@
+def calcular_area(base, altura):
+    """Calcula el área de un rectángulo."""
+    return base * altura
+
+def calcular_perimetro(base, altura):
+    """Calcula el perímetro de un rectángulo."""
+    return 2 * (base + altura)
+
+
+base = 10
+altura = 5
+
+area = calcular_area(base, altura)
+perimetro = calcular_perimetro(base, altura)
+
+print(f"Área: {area}")
+print(f"Perímetro: {perimetro}")
